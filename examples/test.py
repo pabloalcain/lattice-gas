6e50895fff071aaaa57a76b047c83d4fda8a9b62
Pablo Alcain <pabloalcain@gmail.com>
2014-08-08 10:47:42 -0300

    Changed test.py to be quick

--- examples/test.py
+++ examples/test.py
@@ -10,15 +10,6 @@ lat = Lattice(sz, dim = 2)
 pot = Interaction(inter, 1)
 sys = System(lat, pot)
 sys.set_mu(8.0)
-E = []
-N = []
-temp = linspace(4, 1.1, 30)
-for i in temp:
-  print i
-  sys.set_T(i)
-  ee, nn = sys.run(1000 * sz**2)
-  ee, nn = sys.run(1000 * sz**2)
-  E.append(ee)
-  N.append(nn)
-E = array(E); N = array(N)
-M = -(2*N - sz**2)/(sz**2)
+sys.set_T(2.0)
+e, n = sys.run(10 * sz**2)
+print "E = {0}, N = {1}".format(e, n)
